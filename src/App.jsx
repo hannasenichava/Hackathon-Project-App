3d5bed9e715ca29042dee6559b707b27cbf55f6b
Hugo Silva <hugofsilvaa@gmail.com>
2021-03-12 09:01:59 +0000

    draft

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import NavBar from "./Components/NavBar";
+import About from "./Components/About";
+import Footer from "./Components/Footer";
+import Map from "./Components/Map";
 
 function App() {
   return (
-    <div className="App">
-      map
+    <div className='App'>
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route path='/about' component={About} />
+          <Route path='/map' component={Map} />
+        </Switch>
+        <hr />
+        <Footer />
+      </Router>
     </div>
   );
 }
